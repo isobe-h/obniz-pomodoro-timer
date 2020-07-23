899fff5c2962b5168a5798341c57861283c40c14
isobe-h <isobe-h@toack.co.jp>
2020-07-23 14:57:01 +0900

    get rid unused code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import Obniz from 'obniz';
 import { createCanvas } from 'canvas';
 
-const OBNIZ_ID:string = '4666-2889';
-const obniz = new Obniz(OBNIZ_ID);
+const obniz = new Obniz(YOUR_OBNIZ_NUMBER);
 var isBreaktime:boolean = false;
 const initTime = (min: number = 25, sec: number = 0) => {
   var timer = new Date();
@@ -23,15 +22,6 @@ obniz.onconnect = async () => {
     } else if (mainTimer.getSeconds() === 0 && mainTimer.getMinutes() === 0 && isBreaktime) {
       mainTimer = initTime();
       isBreaktime = false;
-    }
-    var state = await obniz.switch?.getWait();
-    console.log(state?.toString());
-    if (state === 'push') {
-      clearInterval(interval);
-    } else if (state === 'left') {
-      interval.refresh();
-    } else if (state === 'right') {
-      mainTimer = initTime(0, 0);
     }
   }, 1000);
 };
